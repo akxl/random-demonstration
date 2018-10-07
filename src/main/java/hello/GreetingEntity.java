e35cc5101dbf3251ddf3069cdf2a0d045d41397c
akxl <aaronleong32@gmail.com>
2018-10-07 18:11:03 +0100

    first commit

---
+++ src/main/java/hello/GreetingEntity.java
@@ -0,0 +1,20 @@
+package hello;
+
+public class GreetingEntity {
+
+    private final long id;
+    private final String content;
+
+    public GreetingEntity(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
